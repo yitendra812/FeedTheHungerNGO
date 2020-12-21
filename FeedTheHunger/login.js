4e86ea6437b828d517ed1a612a5764f8e45a71aa
yitendra812 <60441749+yitendra812@users.noreply.github.com>
2020-12-21 11:50:53 +0530

    Add files via upload

---
+++ FeedTheHunger/login.js
@@ -0,0 +1,24 @@
+var attempt = 5; //Variable to count number of attempts
+
+//Below function Executes on click of login button
+function validate() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username == "Ypushkar" && password == "FeedThePoor") {
+        alert("Login successfully");
+        window.location = "success.html"; //redirecting to other page
+        return false;
+    } else {
+        attempt--; //Decrementing by one
+        alert("Incorrect Password !! You have left " + attempt + " attempt;");
+
+        //Disabling fields after 5 attempts
+        if (attempt == 0) {
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
